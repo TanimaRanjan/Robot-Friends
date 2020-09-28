e0f713e6fc7ceef1013427b5fe68a7695f2b5a88
TanimaRanjan <tanimaranjan@gmail.com>
2020-09-28 14:14:42 -0400

    Adding reducers

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import './index.css';
 import App from './containers/App';
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import thunkMiddleware  from 'redux-thunk';
-import {searchRobots, requestRobot} from "./reducers";
+import {searchRobots, requestRobots} from "./reducers";
 import { createLogger } from 'redux-logger'
 
 
 const logger = createLogger();
 
-const rootReducer = combineReducers({searchRobots, requestRobot})
+const rootReducer = combineReducers({searchRobots, requestRobots})
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(<Provider store={store}>
